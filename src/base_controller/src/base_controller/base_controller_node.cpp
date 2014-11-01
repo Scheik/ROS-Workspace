c1bb5e891080fc24a34dc36015ad5507b6d7892d
Scheik <Scheik@users.noreply.github.com>
2014-11-01 14:18:50 +0100

    Update base_controller_node.cpp

--- src/base_controller/src/base_controller/base_controller_node.cpp
+++ src/base_controller/src/base_controller/base_controller_node.cpp
@@ -1,19 +1,24 @@
-// sourcecode
-// Include the ROS C++ APIs
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
 
-// Standard C++ entry point
-int main(int argc, char** argv) {
-  // Announce this program to the ROS master as a "node" called "hello_world_node"
-  ros::init(argc, argv, "hello_world_node");
-  // Start the node resource managers (communication, time, etc)
-  ros::start();
-  // Broadcast a simple log message
-  ROS_INFO_STREAM("Hello, ROSworld!");
-  // Process ROS callbacks until receiving a SIGINT (ctrl-c)
-  ros::spin();
-  // Stop the node's resources
-  ros::shutdown();
-  // Exit tranquilly
-  return 0;
+void cmd_vel_callback(const geometry_msgs::Twist& vel_cmd)
+{ 
+    ROS_INFO("I heard: [%f]", vel_cmd.linear.y);
+    std::cout << "Twist Received " << std::endl;
+}
+
+
+int main( int argc, char* argv[] )
+{
+ros::init(argc, argv, "toeminator_publisher" );
+
+ros::NodeHandle n;
+ros::Subscriber sub = n.subscribe("/cmd_vel", 1000, cmd_vel_callback);
+
+while( n.ok() ) 
+{
+    ros::spin();
+}
+
+return 1;
 }
