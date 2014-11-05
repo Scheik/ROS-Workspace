d14e561e9f190095f4b3d9f012f01878e2ad8e36
scheik <scheik.todeswache@googlemail.com>
2014-11-05 22:15:18 +0100

    Update code

--- src/base_controller/src/base_controller/base_controller_node.cpp
+++ src/base_controller/src/base_controller/base_controller_node.cpp
@@ -123,11 +123,11 @@ ROS_INFO_STREAM("serial Port opened \n");
 
 while( n.ok() ) 
 {
-    //read_MD49_Data();
-    //usleep(100000);
-    ROS_INFO_STREAM("cycle? \n");
-    ros::spin();
-    //hier code/funktion um md49 daten zu lesen und und auszugeben in konsole und /odom später
+    while(1){
+        read_MD49_Data();
+        usleep(100000);
+        ros::spin();
+    }
 
 }
 
