7f015b1bd11b0b24b587d21e01f0425578cae9c4
scheik <scheik.todeswache@googlemail.com>
2014-11-15 14:55:26 +0100

    Update code

--- src/robot_teleop/src/joystick_driver/joystick_driver_node.cpp
+++ src/robot_teleop/src/joystick_driver/joystick_driver_node.cpp
@@ -7,10 +7,15 @@ int16_t Joy_Button_Up_pressed;
 int16_t Joy_Button_Down_pressed;
 int16_t Joy_Button_Left_pressed;
 int16_t Joy_Button_Right_pressed;
+int16_t Joy_Button_Up_pressed_last=0;
+int16_t Joy_Button_Down_pressed_last=0;
+int16_t Joy_Button_Left_pressed_last=0;
+int16_t Joy_Button_Right_pressed_last=0;
 bool Joy_Set=false;
 
 #define Joystick_Buttons_Left_Right msg->axes[4]
 #define Joystick_Buttons_Up_Down msg->axes[5]
+
 
 void myCallback (const sensor_msgs::Joy::ConstPtr& msg)
 {
@@ -35,44 +40,65 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "joystick_driver");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("joy", 1000, myCallback);
-   //to the /cmd_vel topic, with a queue size of 100
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
-
   //Sets the loop to publish at a rate of 10Hz
   ros::Rate loop_rate(10);
   //Declares the message to be sent
   geometry_msgs::Twist cmd_vel_msg;
 
   while (n.ok()){
-
-    //
-    //cmd_vel_msg.linear.x =2;
-    //cmd_vel_pub.publish(cmd_vel_msg);
-      if (Joy_Button_Up_pressed==1){
-          ROS_INFO("Up");
-          cmd_vel_msg.linear.x=2.0;
+      if (Joy_Button_Up_pressed==1 && Joy_Button_Up_pressed!=Joy_Button_Up_pressed_last){
+          ROS_INFO("Button Up pressed");
+          cmd_vel_msg.linear.x=0.2;
           cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_Button_Up_pressed_last=Joy_Button_Up_pressed;
       }
-      else if (Joy_Button_Down_pressed==-1){
-          ROS_INFO("Down");
-          cmd_vel_msg.linear.x=-2.0;
+      else if (Joy_Button_Down_pressed==-1 && Joy_Button_Down_pressed!=Joy_Button_Down_pressed_last){
+          ROS_INFO("Button Down pressed");
+          cmd_vel_msg.linear.x=-0.2;
           cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_Button_Down_pressed_last=Joy_Button_Down_pressed;
       }
-      else if (Joy_Button_Left_pressed==1){
-          ROS_INFO("Left");
-          cmd_vel_msg.angular.z=2.0;
+      else if (Joy_Button_Left_pressed==1 && Joy_Button_Left_pressed!=Joy_Button_Left_pressed_last){
+          ROS_INFO("Button Left pressed");
+          cmd_vel_msg.angular.z=0.2;
           cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_Button_Left_pressed_last=Joy_Button_Left_pressed;
       }
-      else if (Joy_Button_Right_pressed==-1){
-          ROS_INFO("Right");
-          cmd_vel_msg.angular.z=-2.0;
+      else if (Joy_Button_Right_pressed==-1 && Joy_Button_Right_pressed!=Joy_Button_Right_pressed_last){
+          ROS_INFO("Button Right pressed");
+          cmd_vel_msg.angular.z=-0.2;
           cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_Button_Right_pressed_last=Joy_Button_Right_pressed;
       }
-      else {cmd_vel_msg.linear.x=0;cmd_vel_msg.angular.z=0;cmd_vel_pub.publish(cmd_vel_msg);};
+      else if (Joy_Button_Up_pressed==0 && Joy_Button_Up_pressed!=Joy_Button_Up_pressed_last){
+          ROS_INFO("Button Up released");
+          cmd_vel_msg.linear.x=0.0;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_Button_Up_pressed_last=Joy_Button_Up_pressed;
+      }
+      else if (Joy_Button_Down_pressed==0 && Joy_Button_Down_pressed!=Joy_Button_Down_pressed_last){
+          ROS_INFO("Button Down released");
+          cmd_vel_msg.linear.x=0.0;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_Button_Down_pressed_last=Joy_Button_Down_pressed;
+      }
+      else if (Joy_Button_Left_pressed==0 && Joy_Button_Left_pressed!=Joy_Button_Left_pressed_last){
+          ROS_INFO("Button Left released");
+          cmd_vel_msg.angular.z=0.0;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_Button_Left_pressed_last=Joy_Button_Left_pressed;
+      }
+      else if (Joy_Button_Right_pressed==0 && Joy_Button_Right_pressed!=Joy_Button_Right_pressed_last){
+          ROS_INFO("Button Right released");
+          cmd_vel_msg.angular.z=-0.0;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_Button_Right_pressed_last=Joy_Button_Right_pressed;
+      }
 
     ros::spinOnce();
     loop_rate.sleep();
-  }
+  }//end.mainloop
   return 0;
-}
+}//end.main
 
