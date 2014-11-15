600aef73d486cbf8e3cb96590dd2d456336cdda1
scheik <scheik.todeswache@googlemail.com>
2014-11-15 19:05:20 +0100

    joystick_driver_node.cpp: Add handling of analog stick left from joystick

--- src/robot_teleop/src/joystick_driver/joystick_driver_node.cpp
+++ src/robot_teleop/src/joystick_driver/joystick_driver_node.cpp
@@ -1,8 +1,13 @@
+// ROS Libraries
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Twist.h>
+// Standard Libraries
 #include <stdlib.h>
+#include <math.h>
+// Own Libraries
 
+// Variables
 int16_t Joy_Button_Up_pressed;
 int16_t Joy_Button_Down_pressed;
 int16_t Joy_Button_Left_pressed;
@@ -11,42 +16,58 @@ int16_t Joy_Button_Up_pressed_last=0;
 int16_t Joy_Button_Down_pressed_last=0;
 int16_t Joy_Button_Left_pressed_last=0;
 int16_t Joy_Button_Right_pressed_last=0;
-bool Joy_Set=false;
 
+float Joy_AnalogLeft_Up_pressed;
+float Joy_AnalogLeft_Down_pressed;
+float Joy_AnalogLeft_Left_pressed;
+float Joy_AnalogLeft_Right_pressed;
+float Joy_AnalogLeft_Up_pressed_last=0;
+float Joy_AnalogLeft_Down_pressed_last=0;
+float Joy_AnalogLeft_Left_pressed_last=0;
+float Joy_AnalogLeft_Right_pressed_last=0;
+
+// Defines
 #define Joystick_Buttons_Left_Right msg->axes[4]
 #define Joystick_Buttons_Up_Down msg->axes[5]
+#define Joystick_AnalogLeft_Left_Right msg->axes[0]
+#define Joystick_AnalogLeft_Up_Down msg->axes[1]
 
-
-void myCallback (const sensor_msgs::Joy::ConstPtr& msg)
+// Callback Functions
+void Joy_Callback (const sensor_msgs::Joy::ConstPtr& msg)
 {
-  //for (unsigned i = 0; i < msg->axes.size(); ++i) {
-    //ROS_INFO("Axis %d is now at position %f", i, msg->axes[i]);
-  //}
+    //for (unsigned i = 0; i < msg->axes.size(); ++i) {
+    //  ROS_INFO("Axis %d is now at position %f", i, msg->axes[i]);
+    //}
 
-  // Joystick Button pressed?
-  if (Joystick_Buttons_Left_Right==1) {Joy_Button_Left_pressed=Joystick_Buttons_Left_Right;};
-  if (Joystick_Buttons_Left_Right==-1) {Joy_Button_Right_pressed=Joystick_Buttons_Left_Right;};
-  if (Joystick_Buttons_Up_Down==1) {Joy_Button_Up_pressed=Joystick_Buttons_Up_Down;};
-  if (Joystick_Buttons_Up_Down==-1) {Joy_Button_Down_pressed=Joystick_Buttons_Up_Down;};
-  if (Joystick_Buttons_Up_Down==0) {Joy_Button_Down_pressed=Joystick_Buttons_Up_Down;};
-  if (Joystick_Buttons_Left_Right==0) {Joy_Button_Left_pressed=Joystick_Buttons_Left_Right;};
-  if (Joystick_Buttons_Up_Down==0) {Joy_Button_Up_pressed=Joystick_Buttons_Up_Down;};
-  if (Joystick_Buttons_Left_Right==0) {Joy_Button_Right_pressed=Joystick_Buttons_Left_Right;};
+    // Joystick Analogstick left moved?
+    if (Joystick_AnalogLeft_Left_Right>0) {Joy_AnalogLeft_Left_pressed=Joystick_AnalogLeft_Left_Right;};
+    if (Joystick_AnalogLeft_Left_Right<0) {Joy_AnalogLeft_Right_pressed=Joystick_AnalogLeft_Left_Right;};
+    if (Joystick_AnalogLeft_Up_Down>0) {Joy_AnalogLeft_Up_pressed=Joystick_AnalogLeft_Up_Down;};
+    if (Joystick_AnalogLeft_Up_Down<0) {Joy_AnalogLeft_Down_pressed=Joystick_AnalogLeft_Up_Down;};
+    if (Joystick_AnalogLeft_Up_Down==0) {Joy_AnalogLeft_Down_pressed=Joystick_AnalogLeft_Up_Down;Joy_AnalogLeft_Up_pressed=Joystick_AnalogLeft_Up_Down;};
+    if (Joystick_AnalogLeft_Left_Right==0) {Joy_AnalogLeft_Left_pressed=Joystick_AnalogLeft_Left_Right;Joy_AnalogLeft_Right_pressed=Joystick_AnalogLeft_Left_Right;};
+
+
+    // Joystick Buttons pressed?
+    if (Joystick_Buttons_Left_Right==1) {Joy_Button_Left_pressed=Joystick_Buttons_Left_Right;};
+    if (Joystick_Buttons_Left_Right==-1) {Joy_Button_Right_pressed=Joystick_Buttons_Left_Right;};
+    if (Joystick_Buttons_Up_Down==1) {Joy_Button_Up_pressed=Joystick_Buttons_Up_Down;};
+    if (Joystick_Buttons_Up_Down==-1) {Joy_Button_Down_pressed=Joystick_Buttons_Up_Down;};
+    if (Joystick_Buttons_Up_Down==0) {Joy_Button_Down_pressed=Joystick_Buttons_Up_Down;Joy_Button_Up_pressed=Joystick_Buttons_Up_Down;};
+    if (Joystick_Buttons_Left_Right==0) {Joy_Button_Left_pressed=Joystick_Buttons_Left_Right;Joy_Button_Right_pressed=Joystick_Buttons_Left_Right;};
 }
 
-int main(int argc, char **argv)
-{
-
+int main(int argc, char **argv){
   ros::init(argc, argv, "joystick_driver");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("joy", 1000, myCallback);
+  ros::Subscriber sub = n.subscribe("joy", 1000, Joy_Callback);
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
   //Sets the loop to publish at a rate of 10Hz
   ros::Rate loop_rate(10);
   //Declares the message to be sent
   geometry_msgs::Twist cmd_vel_msg;
-
   while (n.ok()){
+      // PUBLISH to /cmd_vel if Joystick corresponding Buttons are pressed
       if (Joy_Button_Up_pressed==1 && Joy_Button_Up_pressed!=Joy_Button_Up_pressed_last){
           ROS_INFO("Button Up pressed");
           cmd_vel_msg.linear.x=0.2;
@@ -94,6 +115,56 @@ int main(int argc, char **argv)
           cmd_vel_msg.angular.z=-0.0;
           cmd_vel_pub.publish(cmd_vel_msg);
           Joy_Button_Right_pressed_last=Joy_Button_Right_pressed;
+      }
+
+      // PUBLISH to /cmd_vel if AnalogeLeft is moved
+      if (Joy_AnalogLeft_Up_pressed>0 && Joy_AnalogLeft_Up_pressed!=Joy_AnalogLeft_Up_pressed_last){
+          ROS_INFO("AnalogLeft Up changed");
+          cmd_vel_msg.linear.x=floor((Joy_AnalogLeft_Up_pressed*0.2)*100+0.5)/100;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_AnalogLeft_Up_pressed_last=Joy_AnalogLeft_Up_pressed;
+      }
+      else if (Joy_AnalogLeft_Down_pressed<0 && Joy_AnalogLeft_Down_pressed!=Joy_AnalogLeft_Down_pressed_last){
+          ROS_INFO("AnalogLeft Down changed");
+          cmd_vel_msg.linear.x=floor((Joy_AnalogLeft_Down_pressed*0.2)*100+0.5)/100;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_AnalogLeft_Down_pressed_last=Joy_AnalogLeft_Down_pressed;
+      }
+      else if (Joy_AnalogLeft_Left_pressed>0 && Joy_AnalogLeft_Left_pressed!=Joy_AnalogLeft_Left_pressed_last){
+          ROS_INFO("AnalogLeft Left changed");
+          cmd_vel_msg.angular.z=floor((Joy_AnalogLeft_Left_pressed*0.2)*100+0.5)/100;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_AnalogLeft_Left_pressed_last=Joy_AnalogLeft_Left_pressed;
+      }
+      else if (Joy_AnalogLeft_Right_pressed<0 && Joy_AnalogLeft_Right_pressed!=Joy_AnalogLeft_Right_pressed_last){
+          ROS_INFO("AnalogLeft Right changed");
+          cmd_vel_msg.angular.z=floor((Joy_AnalogLeft_Right_pressed*0.2)*100+0.5)/100;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_AnalogLeft_Right_pressed_last=Joy_AnalogLeft_Right_pressed;
+      }
+      else if (Joy_AnalogLeft_Up_pressed==0 && Joy_AnalogLeft_Up_pressed!=Joy_AnalogLeft_Up_pressed_last){
+          ROS_INFO("AnalogLeft Up changed");
+          cmd_vel_msg.linear.x=0.0;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_AnalogLeft_Up_pressed_last=Joy_AnalogLeft_Up_pressed;
+      }
+      else if (Joy_AnalogLeft_Down_pressed==0 && Joy_AnalogLeft_Down_pressed!=Joy_AnalogLeft_Down_pressed_last){
+          ROS_INFO("AnalogLeft Down changed");
+          cmd_vel_msg.linear.x=0.0;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_AnalogLeft_Down_pressed_last=Joy_AnalogLeft_Down_pressed;
+      }
+      else if (Joy_AnalogLeft_Left_pressed==0 && Joy_AnalogLeft_Left_pressed!=Joy_AnalogLeft_Left_pressed_last){
+          ROS_INFO("AnalogLeft Left changed");
+          cmd_vel_msg.angular.z=0.0;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_AnalogLeft_Left_pressed_last=Joy_AnalogLeft_Left_pressed;
+      }
+      else if (Joy_AnalogLeft_Right_pressed==0 && Joy_AnalogLeft_Right_pressed!=Joy_AnalogLeft_Right_pressed_last){
+          ROS_INFO("AnalogLeft Right changed");
+          cmd_vel_msg.angular.z=0.0;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          Joy_AnalogLeft_Right_pressed_last=Joy_AnalogLeft_Right_pressed;
       }
 
     ros::spinOnce();
