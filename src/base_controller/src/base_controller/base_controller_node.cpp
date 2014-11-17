71e80ad714a1490606f4d7939ac4468f8fab6139
scheik <scheik.todeswache@googlemail.com>
2014-11-17 18:41:04 +0100

    Update code

--- src/base_controller/src/base_controller/base_controller_node.cpp
+++ src/base_controller/src/base_controller/base_controller_node.cpp
@@ -11,14 +11,132 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 
+#include <sensor_msgs/JointState.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+
+double width_robot = 0.1;
+double vl = 0.0;
+double vr = 0.0;
+ros::Time last_time;
+double right_enc = 0.0;
+double left_enc = 0.0;
+double right_enc_old = 0.0;
+double left_enc_old = 0.0;
+double distance_left = 0.0;
+double distance_right = 0.0;
+double ticks_per_meter = 100;
+double x = 0.0;
+double y = 0.0;
+double th = 0.0;
+geometry_msgs::Quaternion odom_quat;
 
 struct termios orig;
 int filedesc;
 int fd;
 unsigned char serialBuffer[16];						// Serial buffer to store data for I/O
 
-int openSerialPort(const char * device, int bps)
-{
+int openSerialPort(const char * device, int bps);
+void writeBytes(int descriptor, int count);
+void readBytes(int descriptor, int count);
+void read_MD49_Data (void);
+
+
+
+void cmd_vel_callback(const geometry_msgs::Twist& vel_cmd){ 
+    //ROS_INFO("I heard: [%f]", vel_cmd.linear.y);
+    //std::cout << "Twist Received " << std::endl;
+
+    //ANFANG Alternative
+    double vel_linear_x = vel_cmd.linear.x;
+    double vel_angular_z = vel_cmd.angular.z;
+    double right_vel = 0.0;
+    double left_vel = 0.0;
+
+    if(vel_linear_x == 0){
+        // turning
+        right_vel = vel_angular_z * width_robot / 2.0;
+        left_vel = (-1) * right_vel;
+    }
+    else if(vel_angular_z == 0){
+        // forward / backward
+        left_vel = right_vel = vel_linear_x;
+    }
+    else{
+        // moving doing arcs
+        left_vel = vel_linear_x - vel_angular_z * width_robot / 2.0;
+        right_vel = vel_linear_x + vel_angular_z * width_robot / 2.0;
+    }
+    vl = left_vel;
+    vr = right_vel;
+    //ENDE Alternative
+
+
+    if (vel_cmd.linear.x>0){
+        serialBuffer[0] = 88;							// 88 =X Steuerbyte um Commands an MD49 zu senden
+        serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
+        serialBuffer[2] = 255;					// speed1
+        serialBuffer[3] = 255;					// speed2
+        writeBytes(fd, 4);
+    }
+    if (vel_cmd.linear.x<0){
+        serialBuffer[0] = 88;							// 88 =X Steuerbyte um Commands an MD49 zu senden
+        serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
+        serialBuffer[2] = 0;					// speed1
+        serialBuffer[3] = 0;					// speed2
+        writeBytes(fd, 4);
+    }
+    if (vel_cmd.linear.x==0 && vel_cmd.angular.z==0){
+        serialBuffer[0] = 88;							// 88 =X Steuerbyte um Commands an MD49 zu senden
+        serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
+        serialBuffer[2] = 128;					// speed1
+        serialBuffer[3] = 128;					// speed2
+        writeBytes(fd, 4);
+    }
+    if (vel_cmd.angular.z>0){
+        serialBuffer[0] = 88;							// 88 =X Steuerbyte um Commands an MD49 zu senden
+        serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
+        serialBuffer[2] = 0;					// speed1
+        serialBuffer[3] = 255;					// speed2
+        writeBytes(fd, 4);
+    }
+    if (vel_cmd.angular.z<0){
+        serialBuffer[0] = 88;							// 88 =X Steuerbyte um Commands an MD49 zu senden
+        serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
+        serialBuffer[2] = 255;					// speed1
+        serialBuffer[3] = 0;					// speed2
+        writeBytes(fd, 4);
+    }
+}
+
+
+int main( int argc, char* argv[] ){
+    ros::init(argc, argv, "base_controller" );
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("/cmd_vel", 1000, cmd_vel_callback);
+
+    filedesc = openSerialPort("/dev/ttyAMA0", B38400);
+    if (filedesc == -1) exit(1);
+    usleep(40000);// Sleep for UART to power up and set options
+
+    //ROS_INFO_STREAM("serial Port opened \n");
+    ros::Rate loop_rate(10);
+
+    while( n.ok() )
+    {
+            read_MD49_Data();
+            printf("vl= %f", vl);
+            printf("vr= %f", vr);
+
+
+            ros::spinOnce();
+            loop_rate.sleep();
+    }
+
+    return 1;
+}
+
+int openSerialPort(const char * device, int bps){
    struct termios neu;
    char buf[128];
 
@@ -90,71 +208,4 @@ void read_MD49_Data (void){
     printf("Mode: %i \n",serialBuffer[15]);
     printf("Regulator: %i \n",serialBuffer[16]);
     printf("Timeout: %i \n",serialBuffer[17]);
-}
-
-
-void cmd_vel_callback(const geometry_msgs::Twist& vel_cmd)
-{ 
-    //ROS_INFO("I heard: [%f]", vel_cmd.linear.y);
-    //std::cout << "Twist Received " << std::endl;
-
-    if (vel_cmd.linear.x>0){
-        serialBuffer[0] = 88;							// 88 =X Steuerbyte um Commands an MD49 zu senden
-        serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
-        serialBuffer[2] = 255;					// speed1
-        serialBuffer[3] = 255;					// speed2
-        writeBytes(fd, 4);
-    }
-    if (vel_cmd.linear.x<0){
-        serialBuffer[0] = 88;							// 88 =X Steuerbyte um Commands an MD49 zu senden
-        serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
-        serialBuffer[2] = 0;					// speed1
-        serialBuffer[3] = 0;					// speed2
-        writeBytes(fd, 4);
-    }
-    if (vel_cmd.linear.x==0 && vel_cmd.angular.z==0){
-        serialBuffer[0] = 88;							// 88 =X Steuerbyte um Commands an MD49 zu senden
-        serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
-        serialBuffer[2] = 128;					// speed1
-        serialBuffer[3] = 128;					// speed2
-        writeBytes(fd, 4);
-    }
-    if (vel_cmd.angular.z>0){
-        serialBuffer[0] = 88;							// 88 =X Steuerbyte um Commands an MD49 zu senden
-        serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
-        serialBuffer[2] = 0;					// speed1
-        serialBuffer[3] = 255;					// speed2
-        writeBytes(fd, 4);
-    }
-    if (vel_cmd.angular.z<0){
-        serialBuffer[0] = 88;							// 88 =X Steuerbyte um Commands an MD49 zu senden
-        serialBuffer[1] = 115;							// 115=s Steuerbyte setSpeed
-        serialBuffer[2] = 255;					// speed1
-        serialBuffer[3] = 0;					// speed2
-        writeBytes(fd, 4);
-    }
-}
-
-
-int main( int argc, char* argv[] )
-{
-    ros::init(argc, argv, "base_controller" );
-    ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("/cmd_vel", 1000, cmd_vel_callback);
-
-    filedesc = openSerialPort("/dev/ttyAMA0", B38400);
-    if (filedesc == -1) exit(1);
-    usleep(40000);// Sleep for UART to power up and set options
-
-    //ROS_INFO_STREAM("serial Port opened \n");
-    ros::Rate loop_rate(10);
-
-    while( n.ok() )
-    {
-            read_MD49_Data();
-            ros::spinOnce();
-            loop_rate.sleep();
-    }
-
-    return 1;
 }
