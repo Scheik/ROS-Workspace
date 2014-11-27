ca51fc5d1467268a47caeb9940cb8f69c2cd3982
scheik <scheik.todeswache@googlemail.com>
2014-11-27 19:26:03 +0100

    Update code

--- src/base_controller/src/base_controller/base_controller_node.cpp
+++ src/base_controller/src/base_controller/base_controller_node.cpp
@@ -123,7 +123,7 @@ int main( int argc, char* argv[] ){
 
             // Set speed left andright for MD49
             // ********************************
-            if (cmd_vel_received=true) {set_MD49_speed(speed_l,speed_r); cmd_vel_received=false;}
+            if (cmd_vel_received==true) {set_MD49_speed(speed_l,speed_r); cmd_vel_received=false;}
             //set_MD49_speed(speed_l,speed_r);
 
             // Publish encoder values to topic /encoders (custom message)
