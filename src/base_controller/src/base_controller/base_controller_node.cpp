6a072f022d088eebd046f2f66f17999f08a42dfd
scheik <scheik.todeswache@googlemail.com>
2014-11-28 17:33:39 +0100

    Update code

--- src/base_controller/src/base_controller/base_controller_node.cpp
+++ src/base_controller/src/base_controller/base_controller_node.cpp
@@ -113,7 +113,7 @@ int main( int argc, char* argv[] ){
 
     // Set nodes looprate 10Hz
     // ***********************
-    ros::Rate loop_rate(25);
+    ros::Rate loop_rate(10);
 
     while( n.ok() )
     {
