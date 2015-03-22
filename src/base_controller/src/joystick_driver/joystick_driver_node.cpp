c5a47592a5bab39297c08230a5c9d324c53b6613
user <user@laptopOS.(none)>
2015-03-22 17:34:19 +0100

    update joystick_driver

--- src/base_controller/src/joystick_driver/joystick_driver_node.cpp
+++ src/base_controller/src/joystick_driver/joystick_driver_node.cpp
@@ -6,62 +6,81 @@
 #include <stdlib.h>
 #include <math.h>
 
-double linear_x=0;
-double angular_z=0;
+float linear_x_button=0;
+float angular_z_button=0;
+float linear_x_stick=0;
+float angular_z_stick=0;
+
+bool stick_up_down_pressed=false;
+bool stick_left_right_pressed=false;
+bool button_up_down_pressed=false;
+bool button_left_right_pressed=false;
 
 // Defines
-#define Joystick_Buttons_Left_Right msg->axes[4]
-#define Joystick_Buttons_Up_Down msg->axes[5]
-#define Joystick_AnalogLeft_Left_Right msg->axes[0]
-#define Joystick_AnalogLeft_Up_Down msg->axes[1]
+#define Joystick_Stick_Left_Right msg->axes[4]
+#define Joystick_Stick_Up_Down msg->axes[5]
+#define Joystick_Button_Left_Right msg->axes[0]
+#define Joystick_Button_Up_Down msg->axes[1]
 
 // Callback Functions
 void Joy_Callback (const sensor_msgs::Joy::ConstPtr& msg)
 {
-/*    for (unsigned i = 0; i < msg->axes.size(); ++i) {
-      ROS_INFO("Axis %d is now at position %f", i, msg->axes[i]);
-    }
-*/
+//  for (unsigned i = 0; i < msg->axes.size(); ++i) {
+//      ROS_INFO("Axis %d is now at position %f", i, msg->axes[i]);
+//  }
 
-    // Joystick Buttons pressed?
-    if (Joystick_Buttons_Left_Right==1) {
-        angular_z=Joystick_Buttons_Left_Right;
+    // Test if joystick buttons are pressed?
+    if (Joystick_Button_Left_Right>0) {
+        angular_z_button=Joystick_Button_Left_Right;
+        button_left_right_pressed=true;
     }
-    if (Joystick_Buttons_Left_Right==-1) {
-        angular_z=Joystick_Buttons_Left_Right;
+    if (Joystick_Button_Left_Right<0) {
+        angular_z_button=Joystick_Button_Left_Right;
+        button_left_right_pressed=true;
     }
-    if (Joystick_Buttons_Up_Down==1) {
-        linear_x=Joystick_Buttons_Up_Down;
+    if (Joystick_Button_Up_Down>0) {
+        linear_x_button=Joystick_Button_Up_Down/5;
+        button_up_down_pressed=true;
     }
-    if (Joystick_Buttons_Up_Down==-1) {
-        linear_x=Joystick_Buttons_Up_Down;
+    if (Joystick_Button_Up_Down<0) {
+        linear_x_button=Joystick_Button_Up_Down/5;
+        button_up_down_pressed=true;
     }
-    if (Joystick_Buttons_Up_Down==0) {
-        linear_x=Joystick_Buttons_Up_Down;
+    if (Joystick_Button_Up_Down==0) {
+        linear_x_button=Joystick_Button_Up_Down;
+        button_up_down_pressed=false;
     }
-    if (Joystick_Buttons_Left_Right==0) {
-        angular_z=Joystick_Buttons_Up_Down;
+    if (Joystick_Button_Left_Right==0) {
+        angular_z_button=Joystick_Button_Left_Right;
+        button_left_right_pressed=false;
     }
 
-    // Joystick Analog pressed?
-    if (Joystick_AnalogLeft_Left_Right>0) {
-        angular_z=Joystick_AnalogLeft_Left_Right;
+    // Test if joystick stick is pressed?
+    if (Joystick_Stick_Left_Right>0) {
+        angular_z_stick=Joystick_Stick_Left_Right;
+        stick_left_right_pressed=true;
     }
-    if (Joystick_AnalogLeft_Left_Right<0) {
-        angular_z=Joystick_AnalogLeft_Left_Right;
+    if (Joystick_Stick_Left_Right<0) {
+        angular_z_stick=Joystick_Stick_Left_Right;
+        stick_left_right_pressed=true;
     }
-    if (Joystick_AnalogLeft_Up_Down>0) {
-        linear_x=Joystick_AnalogLeft_Up_Down;
+    if (Joystick_Stick_Up_Down>0) {
+        linear_x_stick=Joystick_Stick_Up_Down/5;
+        stick_up_down_pressed=true;
     }
-    if (Joystick_AnalogLeft_Up_Down<0) {
-        linear_x=Joystick_AnalogLeft_Up_Down;
+    if (Joystick_Stick_Up_Down<0) {
+        linear_x_stick=Joystick_Stick_Up_Down/5;
+        stick_up_down_pressed=true;
     }
-    if (Joystick_AnalogLeft_Up_Down==0) {
-        linear_x=Joystick_AnalogLeft_Up_Down;
+    if (Joystick_Stick_Up_Down==0) {
+        linear_x_stick=Joystick_Stick_Up_Down;
+        stick_up_down_pressed=false;
     }
-    if (Joystick_AnalogLeft_Left_Right==0) {
-        angular_z=Joystick_AnalogLeft_Left_Right;
+    if (Joystick_Stick_Left_Right==0) {
+        angular_z_stick=Joystick_Stick_Left_Right;
+        stick_left_right_pressed=false;
     }
+
 
 }
 
@@ -71,31 +90,39 @@ void onExit( void )
     ROS_INFO("joystick_driver: Exit node");
 }
 
-int main(int argc, char **argv){
-
-    atexit(onExit);
-
-    ros::init(argc, argv, "joystick_driver");
+int main(int argc, char **argv)
+{
+  atexit(onExit);
+  ros::init(argc, argv, "joystick_driver");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("joy", 100, Joy_Callback);
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 100);
   ros::Rate loop_rate(50);                                                  //Sets the loop to publish at a rate of 10Hz
   geometry_msgs::Twist cmd_vel_msg;                                         //Declares the message to be sent
+  while (n.ok()){   
 
-  while (n.ok()){
-
-      if ((cmd_vel_msg.linear.x != linear_x) || (cmd_vel_msg.angular.z != angular_z)){
-          cmd_vel_msg.linear.x=linear_x;
-          cmd_vel_msg.angular.z=angular_z;
+    if ((stick_left_right_pressed==false) && (stick_up_down_pressed==false))
+    {
+        if ((cmd_vel_msg.linear.x != linear_x_button) || (cmd_vel_msg.angular.z != angular_z_button)){
+          cmd_vel_msg.linear.x=linear_x_button;
+          cmd_vel_msg.angular.z=angular_z_button;
           cmd_vel_pub.publish(cmd_vel_msg);
           ROS_INFO("linear.x = %f angular.z = %f",cmd_vel_msg.linear.x,cmd_vel_msg.angular.z);
-      }
-
+        }
+    }
+    if ((button_left_right_pressed==false) && (button_up_down_pressed==false))
+    {
+        if ((cmd_vel_msg.linear.x != linear_x_stick) || (cmd_vel_msg.angular.z != angular_z_stick)){
+          cmd_vel_msg.linear.x=linear_x_stick;
+          cmd_vel_msg.angular.z=angular_z_stick;
+          cmd_vel_pub.publish(cmd_vel_msg);
+          ROS_INFO("linear.x = %f angular.z = %f",cmd_vel_msg.linear.x,cmd_vel_msg.angular.z);
+        }
+    }
 
     ros::spinOnce();
     loop_rate.sleep();
   }//end.mainloop
-
   return 0;
 }//end.main
 
