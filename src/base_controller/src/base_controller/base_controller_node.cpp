fab76d5145a5f8b24689644d994a8780ce52fad9
user <user@workstationOS.(none)>
2015-03-29 18:24:24 +0200

    Update analog speed_l and speed_r

--- src/base_controller/src/base_controller/base_controller_node.cpp
+++ src/base_controller/src/base_controller/base_controller_node.cpp
@@ -39,18 +39,18 @@ int last_speed_l=128, last_speed_r=128;                     // buffers last set 
 char reply[8];                                              // max buffersize serial input
 
 void cmd_vel_callback(const geometry_msgs::Twist& vel_cmd){
-    if (vel_cmd.linear.x>0){
+    if (vel_cmd.linear.x != 0){
         //speed_l = 255;
         //speed_r = 255;
         speed_l = 128+(635*vel_cmd.linear.x);
         speed_r = 128+(635*vel_cmd.linear.x);
     }
-    if (vel_cmd.linear.x<0){
+    //if (vel_cmd.linear.x<0){
         //speed_l = 0;
         //speed_r = 0;
-        speed_l = 128+(635*vel_cmd.linear.x);
-        speed_r = 128+(635*vel_cmd.linear.x);
-    }
+     //   speed_l = 128+(635*vel_cmd.linear.x);
+     //   speed_r = 128+(635*vel_cmd.linear.x);
+    //}
     if (vel_cmd.linear.x==0 && vel_cmd.angular.z==0){
         speed_l = 128;
         speed_r = 128;
