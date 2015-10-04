93d14e6908410ffa27491252e1e9368469e5b3f6
user <scheik.todeswache@googlemail.com>
2015-10-04 13:04:11 +0200

    Change arduino board from first example from mega to nano

--- src/arduino_sourcecodes/firmware/arduino_nano1_firmware.cpp
+++ src/arduino_sourcecodes/firmware/arduino_nano1_firmware.cpp
@@ -5,20 +5,20 @@
 ros::NodeHandle nh;
 
 std_msgs::String str_msg;
-ros::Publisher arduino_mega1("chatter", &str_msg);
+ros::Publisher arduino_nano1("chatter", &str_msg);
 
 char hello[13] = "hello world!";
 
 void setup()
 {
   nh.initNode();
-  nh.advertise(arduino_mega1);
+  nh.advertise(arduino_nano1);
 }
 
 void loop()
 {
   str_msg.data = hello;
-  arduino_mega1.publish( &str_msg );
+  arduino_nano1.publish( &str_msg );
   nh.spinOnce();
   delay(1000);
 }
