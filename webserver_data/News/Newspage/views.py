5f4fee683778f2c253a6089fd22454922ea719fd
user <scheik.todeswache@googlemail.com>
2015-11-26 16:52:26 +0100

    REMOVE webserver_data, moved webserver data to separate repository

--- webserver_data/News/Newspage/views.py
+++
@@ -1,8 +0,0 @@
-#from django.http import HttpResponse
-from django.shortcuts import render
-from .models import posts
-
-def newspage(request):
-	entries = posts.objects.all()[:100]
-	return render(request, "newspage.html",{ 'posts' : entries })
-
--- webserver_data/README.md
+++
@@ -1,1 +0,0 @@
-This folder is woorking as root directory for nginx (running on workstationOS).
