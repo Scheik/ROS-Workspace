980fb03a5366ca260641fc0caecc882925a915ea
user <scheik.todeswache@googlemail.com>
2015-12-28 13:27:55 +0100

    UPDATE example_simple_navigation_goals.cpp: Node used frame base_link instead of base_footprint

--- src/robot_2dnav/src/example_simple_navigation_goals.cpp
+++ src/robot_2dnav/src/example_simple_navigation_goals.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv){
   move_base_msgs::MoveBaseGoal goal;
 
   //we'll send a goal to the robot to move 1 meter forward
-  goal.target_pose.header.frame_id = "base_link";
+  goal.target_pose.header.frame_id = "base_footprint";
   goal.target_pose.header.stamp = ros::Time::now();
 
   goal.target_pose.pose.position.x = 1.0;
