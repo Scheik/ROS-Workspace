471f101a24398fdfd8bfb7000965e7e1d215fe93
scheik <scheik.todeswache@googlemail.com>
2017-11-05 12:44:10 +0100

    debug

--- src/base_controller/include/base_controller/base_controller_class.h
+++ src/base_controller/include/base_controller/base_controller_class.h
@@ -279,8 +279,8 @@ void BaseController::cmd_vel_callback(const geometry_msgs::Twist& vel_cmd)
     // Turn clock- or counterclockwise:
     if (vel_cmd.angular.z != 0 && vel_cmd.linear.x==0){
 				// MD49 Mode = 0 *************************************
-				requested_speed_l = 128 - (faktor_linear_x*vel_cmd.angular.z);
-			  requested_speed_r = 128 + (faktor_linear_x*vel_cmd.angular.z);
+				requested_speed_l = 128 - (127*vel_cmd.angular.z);
+			  requested_speed_r = 128 + (127*vel_cmd.angular.z);
 
 				// MD49 Mode = 2 *************************************
 				//requested_speed_l = 128-(127*vel_cmd.angular.z);
